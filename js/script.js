f7056fe02b9182cb44196e1afa301bd60a274160
Stefano-Para <stefano.zina@gmail.com>
2021-05-10 16:35:53 +0200

    problema "nomi + cognomi insieme" da stampare

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var studenti =
+[
+    {
+        nome: "Stefano",
+        cognome: "Zina",
+        eta: 29,
+    },
+
+    {
+        nome: "Giovanni" ,
+        cognome: "Rana",
+        eta: 83,
+    },
+
+    {
+        nome: "Danny",
+        cognome: "DeVito",
+        eta: 76,
+    },
+
+    {
+        nome: "Aldo",
+        cognome: "Baglio",
+        eta: 62,
+    }
+];
+console.log("Mostra ciò che è contenuto in [studenti]:");
+console.log(studenti)
+
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+var arrayNomi = [];
+var arrayCognomi = [];
+var arrayEta = [];
+
+
+for (i = 0; i < studenti.length ; i++) {
+    var totaleOggetti = studenti[i];
+    console.log("Proprietà oggetto stampate:")
+    console.log(totaleOggetti);
+
+    // Creare un array di oggetti di studenti.
+    var totaleNomi = studenti[i].nome;
+    console.log("Nomi singoli da mettere in array: " + totaleNomi);
+    arrayNomi.push(totaleNomi);
+
+    var totaleCognomi = studenti[i].cognome;
+    console.log("Cognomi singoli da mettere in array: " + totaleCognomi);
+    arrayCognomi.push(totaleCognomi);
+
+    var totaleEta = studenti[i].eta;
+    console.log("Eta singole da mettere in array: " + totaleEta + "\n ");
+    arrayEta.push(totaleEta);
+  
+}
+console.log(arrayNomi);
+console.log(arrayCognomi);
+console.log(arrayEta);
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (i = 0; i < studenti.length; i++){
+    var prova = studenti[i];
+    var nomiCognomi = prova['nome']['cognome'];
+    console.log(nomiCognomi);
+    
+}
+
+
+// Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
